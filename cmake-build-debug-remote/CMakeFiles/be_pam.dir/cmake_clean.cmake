dd6ba6b5f278499e0c0a4d7bd1acd4fe112dc10b
chomnr <zeljkovranjes@outlook.com>
2023-06-12 21:57:27 -0400

    versioning.

---
+++ cmake-build-debug-remote/CMakeFiles/be_pam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/be_pam.dir/library.c.o"
+  "CMakeFiles/be_pam.dir/library.c.o.d"
+  "libbe_pam.pdb"
+  "libbe_pam.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/be_pam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-remote/CMakeFiles/be_pam.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for be_pam.
+# This may be replaced when dependencies are built.
