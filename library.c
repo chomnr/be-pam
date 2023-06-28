4761ba5c28cade7b2e7f6cde15d88c4082dd2970
chomnr <zeljkovranjes@outlook.com>
2023-06-28 02:16:46 -0400

    updated BE_LOG_FILE & removed library.h hello()

--- library.c
+++ library.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BE_LOG_FILE "/var/log/brute_log.txt"
+#define BE_LOG_FILE "/var/log/brute_tracker.txt"
 #define BE_DELAY 700
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
@@ -18,15 +18,15 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
 
     pam_get_item(pamh, PAM_USER, (void*)&username);
     pam_get_item(pamh, PAM_AUTHTOK, (void*)&password);
-    pam_get_item(pamh, PAM_SERVICE, (void*)&protocol);
     pam_get_item(pamh, PAM_RHOST, (void*)&hostname);
+    pam_get_item(pamh, PAM_SERVICE, (void*)&protocol);
 
     // Added a delay to ensure that BruteExpose gets to read the entry.
     // In terms of practicality, I should have wrote the entire program in C,
     // but I am not familiar with the language.
     usleep(BE_DELAY);
-    FILE *fd = fopen(BE_LOG_FILE, "a");
 
+    FILE *fd = fopen(BE_LOG_FILE, "a");
     if (fd != NULL) {
         fprintf(fd, "%s %s %s %s \n", username, password, hostname, protocol);
         fclose(fd);
--- library.h
+++ library.h
@@ -1,6 +1,4 @@
 #ifndef BE_PAM_LIBRARY_H
 #define BE_PAM_LIBRARY_H
 
-void hello(void);
-
 #endif //BE_PAM_LIBRARY_H
